65821d6abc22cae9638e0f16a587470dbd5b6d9f
omaroium <omaralimycan@gmail.com>
2022-08-05 10:24:38 +0300

    with db

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 Config = {
+  "apiKey": "AIzaSyB1nL_aDIG_HdYXfVXj_oqLXJjSxnzUPCk",
+  "authDomain": "entrpxcs.firebaseapp.com",
+  "projectId": "entrpxcs",
+  "storageBucket": "entrpxcs.appspot.com",
+  "messagingSenderId": "200107293232",
+  "appId": "1:200107293232:web:77d0ecb423c4e82ed90bf5",
+  "measurementId": "G-LT58HQDH4E"
+  "databaseURL":"https://entrpxcs-default-rtdb.europe-west1.firebasedatabase.app/"
 
 }
 firebase=pyrebase.initialize_app(Config)
